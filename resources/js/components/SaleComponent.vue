7d03bc37c55d78edef1bbf653a10abc99ec64ff2
KevinJoseph <kevramos95@gmail.com>
2019-10-06 16:07:31 -0500

    onclik po change}

--- resources/js/components/SaleComponent.vue
+++ resources/js/components/SaleComponent.vue
@@ -29,9 +29,9 @@
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">Platos</label>
-                    <select class="form-control" v-model="sale.food_id" v-on:click="addPrice(sale.food_id)">
+                    <select class="form-control" v-model="sale.food_id" @change="addPrice(sale.food_id)">
                         <option>Seleccionar un Plato</option>    
-                        <option v-for="(food,index) in foods" v-bind:key="index" v-on:click="addPrice(sale.food_id)" :value="food.id" >{{food.name}}</option>
+                        <option v-for="(food,index) in foods" v-bind:key="index" @change="addPrice(sale.food_id)" :value="food.id" >{{food.name}}</option>
                     </select>  
                 </div>
                 <div class="form-group">
